b792f9776f2ddad5cc6bfaaddc7a7fa28c2b655e
Ismail Tabtabai <ismail@tabtabai.org>
2023-08-09 09:19:17 +0300

    Revenue reports

---
+++ gymasy/gym_management/report/gymasy_revenue_by_month/gymasy_revenue_by_month.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ismail Tabtabai and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Gymasy Revenue By Month"] = {
+	"filters": [
+
+	]
+};
